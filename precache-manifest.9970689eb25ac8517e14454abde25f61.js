08d364368af052c59876933a9bcf0f5fceb3a92a
paulinaniemczyk <floryan.paula@gmail.com>
2019-01-19 13:13:57 +0100

    Updates

---
+++ precache-manifest.9970689eb25ac8517e14454abde25f61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f80ae3bf1cbcda5dad1",
+    "url": "/random/static/js/runtime~main.5f80ae3b.js"
+  },
+  {
+    "revision": "02773a25c28ae72a1cf7",
+    "url": "/random/static/js/main.02773a25.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/random/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "02773a25c28ae72a1cf7",
+    "url": "/random/static/css/main.526e7847.chunk.css"
+  },
+  {
+    "revision": "68cb53deb16819cc41ad4d9d48a93bac",
+    "url": "/random/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/random/precache-manifest.cafc77390b733dc017479670afcf67ec.js"
+  "/random/precache-manifest.9970689eb25ac8517e14454abde25f61.js"
 );
 
 workbox.clientsClaim();
